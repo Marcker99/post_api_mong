3cc9fee7c1b1df08d32648582bc33fc93a7e5027
Andrew Marcker <ticey1995@gmail.com>
2023-03-25 14:12:06 +0200

    fixed return

--- src/middleWares/auth.middleware.ts
+++ src/middleWares/auth.middleware.ts
@@ -1,17 +1,16 @@
-import {NextFunction,Response,Request} from "express";
+import {NextFunction, Response, Request} from "express";
 
 
-
-
-const startPass:string = Buffer.from('admin:qwerty').toString('base64')
+const startPass: string = Buffer.from('admin:qwerty').toString('base64')
 const authPass: string = `Basic ${startPass}`
 //Buffer.from(authHeader.split(' ')[1], 'base64').toString() 
 
-export const authMiddleWare = (req:Request,res:Response,next:NextFunction)=>{
-        if(!req.headers.authorization || req.headers.authorization !== authPass){
-                res.send(401)
-        }
-        next()
+export const authMiddleWare = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.headers.authorization || req.headers.authorization !== authPass) {
+        res.send(401)
+        return
+    }
+    next()
 }
 
 
